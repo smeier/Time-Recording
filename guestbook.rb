a527350eeac141e07365692b8bdb43e3ddeb4546
Stefan Meier <stefan@samsung.(none)>
2009-11-22 22:15:17 +0100

    now with autocomplete

--- guestbook.rb
+++ guestbook.rb
@@ -8,20 +8,20 @@ DataMapper.setup(:default, "appengine://auto")
 
 # Create your model classes
 class Shout
-  include DataMapper::Resource
+    include DataMapper::Resource
 
-  property :id, Serial
-  property :message, Text
+    property :id, Serial
+    property :message, Text
 end
 
 class TRItem
-  include DataMapper::Resource
+    include DataMapper::Resource
 
-  property :id, Serial
-  property :date, Date
-  property :duration, Integer
-  property :project, Text
-  property :message, Text
+    property :id, Serial
+    property :date, Date
+    property :duration, Integer
+    property :project, Text
+    property :message, Text
 end
 
 # Set Haml output format and enable escapes
@@ -29,27 +29,65 @@ set :haml, {:format => :html5 , :escape_html => true }
 
 # time record board
 get '/' do
-  # Just list all the time record items
-  @tritems = TRItem.all
-  @projects = getProjectsFrom(@tritems)
-  
-  haml :index
+    # Just list all the time record items
+    @tritems = TRItem.all
+    @projects = getProjectsFrom(@tritems)
+    @messages = getMessagesFrom(@tritems)
+    
+    haml :index
 end
 
 post '/' do
-  # Create a now shout and redirect back to the list
-  tritem = TRItem.create(:message => params[:message],
-                         :date => params[:date],
-                         :project => params[:project],
-                         :duration => params[:duration])
-  redirect '/'
+    # Create a now shout and redirect back to the list
+    tritem = TRItem.create(:message => params[:message],
+                                                 :date => params[:date],
+                                                 :project => params[:project],
+                                                 :duration => params[:duration])
+    redirect '/'
+end
+
+get '/projects' do
+    # Just list all the time record items
+    @tritems = TRItem.all
+    projects = getProjectsFrom(@tritems)
+    
+    haml :list, :layout => false, :locals => {:items => projects}
+end
+
+
+get '/messages' do
+    # Just list all the time record items
+    @tritems = TRItem.all
+    messages = getMessagesFrom(@tritems)
+    
+    haml :list, :layout => false, :locals => {:items => messages}
 end
 
 
 def getProjectsFrom(items) 
-  projects = {}
-  for item in items do
-    projects[item.project] = item.project
-  end
-  
+    projects = {}
+    for item in items do
+        puts item.project
+        projects[item.project] = item.project
+    end
+    projects
 end
+
+def getMessagesFrom(items)
+    messages = {}
+    for item in items do
+        messages[item.message] = item.message
+    end
+    # TODO:
+    # getSetOfFieldsFromList(items, "message")
+    messages
+end
+
+def getSetOfFieldsFromList(items, fieldname)
+    result = {}
+    for item in items do
+        result[item.attr("message")] = 1
+    end
+    result
+end 
+
